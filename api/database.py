8255770b22d32c060c543abecd02fcb97517db60
NoXei <tahalasfar5544@gmail.com>
2026-05-08 21:23:22 +0100

    fix: load environment variables and add defaults to database.py

--- api/database.py
+++ api/database.py
@@ -2,12 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from os import getenv
+from dotenv import load_dotenv
 
-DB_HOST = getenv("DB_HOST")
-DB_NAME = getenv("DB_NAME")
-DB_USER = getenv("DB_USER")
-DB_PASSWORD = getenv("DB_PASSWORD")
-DB_PORT = getenv("DB_PORT")
+load_dotenv()
+
+DB_HOST = getenv("DB_HOST", "localhost")
+DB_NAME = getenv("DB_NAME", "hackathon_db")
+DB_USER = getenv("DB_USER", "postgres")
+DB_PASSWORD = getenv("DB_PASSWORD", "postgres")
+DB_PORT = getenv("DB_PORT", "5432")
 
 URL_DATABASE = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(URL_DATABASE)
