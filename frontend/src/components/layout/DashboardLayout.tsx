a181512cc051ca882a889fd95884ca0316448192
NoXei <tahalasfar5544@gmail.com>
2026-05-09 20:29:28 +0100

    refactor: align DashboardLayout background with theme

--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import { Header } from './Header';
 
 export function DashboardLayout() {
   return (
-    <div className="flex min-h-screen bg-slate-50">
+    <div className="flex min-h-screen bg-background">
       <Sidebar />
       <div className="flex-1 flex flex-col min-h-screen">
         <Header />
