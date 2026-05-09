c33918d5e66da948231abbb3598b5e6adf762f13
NoXei <tahalasfar5544@gmail.com>
2026-05-09 20:28:45 +0100

    refactor: add getUserDisplayName utility

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getUserDisplayName(user: { full_name?: string | null; email: string } | null | undefined) {
+  if (!user) return "User";
+  return user.full_name || user.email.split('@')[0];
+}
